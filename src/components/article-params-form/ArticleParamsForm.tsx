dd4ddc81f4c0f1d71d11aba4cd45bfaf674c8e09
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-04 18:46:45 +0300

    refactor: remove redundant onClose from useOutsideClickClose call

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -32,7 +32,6 @@ export const ArticleParamsForm = ({ onApply }: ArticleParamsFormProps) => {
 	useOutsideClickClose({
 		isOpen,
 		rootRef,
-		onClose: () => setIsOpen(false),
 		onChange: setIsOpen,
 	});
 
